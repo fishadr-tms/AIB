fb44b1a804f438222f38827a71c1d0e5237817c5
Adrian Fish <adrian.fish@trustmarque.com>
2022-02-21 15:17:37 +0000

    Use Winget

--- AIBWindowsSWinstall.ps1
+++ AIBWindowsSWinstall.ps1
@@ -5,10 +5,11 @@ New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
 
 #Install VSCode
 Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?Linkid=852157' -OutFile 'c:\temp\VScode.exe'
-Invoke-Expression -Command 'c:\temp\VScode.exe /verysilent'
+Invoke-Expression -Command 'winget install vscode'
+#Invoke-Expression -Command 'c:\temp\VScode.exe /verysilent'
 
 #Start sleep
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 360
 
 #InstallNotepadplusplus
 #Invoke-WebRequest -Uri 'https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe' -OutFile 'c:\temp\notepadplusplus.exe'
