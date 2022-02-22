19b60eaaad106825c6af302bdb721ad5a118f7f7
Adrian Fish <adrian.fish@trustmarque.com>
2022-02-22 11:10:07 +0000

    Updated

--- AIBWindowsSWinstall.ps1
+++ AIBWindowsSWinstall.ps1
@@ -3,21 +3,25 @@
 #Create temp folder
 New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
 
+#Install PowerBI Desktop
+Invoke-WebRequest -Uri 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=58494&6B49FDFB-8E5B-4B07-BC31-15695C5A2143=1' -OutFile 'c:\temp\PBIDesktopSetup_x64.exe'
+Invoke-Expression -Command 'c:\temp\PBIDesktopSetup_x64.exe /SILENT /NORESTART /ACCEPT_EULA=1'
+#Start sleep
+Start-Sleep -Seconds 60
+
 #Install VSCode
 Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?Linkid=852157' -OutFile 'c:\temp\VScode.exe'
-#Start-Process -FilePath "c:\temp\VScode.exe" -ArgumentList "/quiet" -Wait 
-#Start-Process -FilePath "c:\temp\VScode.exe" -ArgumentList "/quiet" -Wait 
 Invoke-Expression -Command 'c:\temp\VScode.exe /VERYSILENT /NORESTART /MERGETASKS=!runcode'
-
 #Start sleep
-Start-Sleep -Seconds 360
+Start-Sleep -Seconds 60
 
 #InstallNotepadplusplus
 Invoke-WebRequest -Uri 'https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe' -OutFile 'c:\temp\notepadplusplus.exe'
 Invoke-Expression -Command 'c:\temp\notepadplusplus.exe /S'
-
 #Start sleep
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 60
+
+
 
 #InstallFSLogix
 #Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile 'c:\temp\fslogix.zip'
